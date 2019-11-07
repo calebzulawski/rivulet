a701f7b435eba1c9da1546f688ce02f5dfba4e19
Caleb Zulawski <caleb.zulawski@gmail.com>
2019-11-06 23:23:04 -0500

    Fix test

--- tests/spsc_buffer.rs
+++ tests/spsc_buffer.rs
@@ -7,22 +7,26 @@ static BUFFER_SIZE: usize = 4096;
 
 #[tokio::test]
 async fn spsc_buffer_integrity() {
-    async fn write(mut sink: spsc::BufferSink<i64>, block: usize, count: usize) -> u64 {
+    async fn write(mut sink: spsc::BufferSink<i64>, block: usize, count: usize) -> (usize, u64) {
         let mut hasher = seahash::SeaHasher::new();
         let mut rng = rand::thread_rng();
+        let mut total = 0;
         for _ in 0..count {
             sink = sink.next(block).await.unwrap();
             for value in sink.iter_mut() {
                 *value = rng.gen();
                 hasher.write_i64(*value);
             }
+            total += block;
         }
-        return hasher.finish();
+        sink.advance(block, 0).await.unwrap();
+        return (total, hasher.finish());
     }
     
-    async fn read(mut source: spsc::BufferSource<i64>) -> u64 {
+    async fn read(mut source: spsc::BufferSource<i64>) -> (usize, u64) {
         let mut hasher = seahash::SeaHasher::new();
         let mut rng = rand::thread_rng();
+        let mut total = 0;
         loop {
             let block = rng.gen_range(1, BUFFER_SIZE);
             source = match source.next(block).await {
@@ -30,9 +34,10 @@ async fn spsc_buffer_integrity() {
                     for value in source.iter() {
                         hasher.write_i64(*value);
                     }
+                    total += source.len();
                     source
                 },
-                None => return hasher.finish()
+                None => return (total, hasher.finish())
             };
         }
     }
