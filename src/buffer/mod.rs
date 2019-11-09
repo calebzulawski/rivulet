5dedc84d57a09cae4fcfc787a18b158a4fbe8441
Caleb Zulawski <caleb.zulawski@gmail.com>
2019-11-09 15:54:27 -0500

    Make common spsc/spmc buffer implementation

--- src/buffer/mod.rs
+++ src/buffer/mod.rs
@@ -3,7 +3,11 @@
 pub mod spsc;
 
 use slice_deque::Buffer;
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::{
+    atomic::{AtomicUsize, Ordering},
+    Arc, Mutex, RwLock,
+};
+use tokio::sync::mpsc::{Receiver, Sender};
 
 struct State<T> {
     buffer: Buffer<T>,
@@ -42,14 +46,13 @@ impl<T> State<T> {
         unsafe { self.buffer.ptr().add(self.tail.load(Ordering::Relaxed)) }
     }
 
-    fn advance_tail(&self, advance: usize) {
-        let new_tail = (self.tail.load(Ordering::Relaxed) + advance) % self.size;
-        self.tail.store(new_tail, Ordering::Relaxed);
+    fn advance_value(&self, advance: usize, value: &AtomicUsize) {
+        let new_value = (value.load(Ordering::Relaxed) + advance) % self.size;
+        value.store(new_value, Ordering::Relaxed);
     }
 
-    fn advance_head(&self, advance: usize) {
-        let new_head = (self.head.load(Ordering::Relaxed) + advance) % self.size;
-        self.head.store(new_head, Ordering::Relaxed);
+    fn distance(&self, first: usize, second: usize) -> usize {
+        (second + self.size - first) % self.size
     }
 
     fn writable_len(&self) -> usize {
@@ -57,7 +60,170 @@ impl<T> State<T> {
     }
 
     fn readable_len(&self) -> usize {
-        (self.tail.load(Ordering::Relaxed) + self.size - self.head.load(Ordering::Relaxed))
-            % self.size
+        self.distance(
+            self.head.load(Ordering::Relaxed),
+            self.tail.load(Ordering::Relaxed),
+        )
+    }
+}
+
+enum MultiSender {
+    Single(Box<Sender<()>>),
+    Multiple(Arc<Mutex<Vec<Sender<()>>>>),
+}
+
+struct SinkImpl<T> {
+    state: Arc<State<T>>,
+    trigger_receiver: Receiver<()>,
+    trigger_sender: MultiSender,
+    size: usize,
+}
+
+impl<T> SinkImpl<T> {
+    fn len(&self) -> usize {
+        self.size
+    }
+
+    async fn advance(&mut self, advance: usize, size: usize) -> Option<&mut [T]> {
+        assert!(
+            advance <= self.size,
+            "cannot advance past end of write buffer"
+        );
+        assert!(
+            size <= (self.state.size - 1),
+            "cannot request write buffer larger than total buffer"
+        );
+        self.state.advance_value(advance, &self.state.tail);
+
+        // Notify source(s) that data is available
+        // If the source is gone, there's no point sinking more data
+        let connected = match &mut self.trigger_sender {
+            MultiSender::Single(sender) => {
+                if let Err(e) = sender.try_send(()) {
+                    !e.is_closed()
+                } else {
+                    true
+                }
+            }
+            MultiSender::Multiple(senders) => {
+                let mut senders = senders.lock().unwrap();
+                for idx in (0..senders.len()).rev() {
+                    if let Err(e) = senders[idx].try_send(()) {
+                        if e.is_closed() {
+                            senders.remove(idx);
+                        }
+                    }
+                }
+                !senders.is_empty()
+            }
+        };
+        if !connected {
+            self.size = 0;
+            return None;
+        }
+
+        // Wait for enough room to advance
+        while self.state.writable_len() < size {
+            if self.trigger_receiver.recv().await.is_none() {
+                self.size = 0;
+                return None;
+            }
+        }
+        self.size = size;
+
+        // Return the mutable slice
+        let ptr = self.state.tail_ptr();
+        Some(unsafe { std::slice::from_raw_parts_mut(ptr, self.size) })
+    }
+}
+
+struct MultiSource {
+    head: Arc<AtomicUsize>,
+    heads: Arc<RwLock<Vec<Arc<AtomicUsize>>>>,
+    senders: Arc<Mutex<Vec<Sender<()>>>>,
+}
+
+struct SourceImpl<T> {
+    state: Arc<State<T>>,
+    size: usize,
+    trigger_receiver: Receiver<()>,
+    trigger_sender: Sender<()>,
+    multi_source: Option<MultiSource>,
+}
+
+impl<T> SourceImpl<T> {
+    fn len(&self) -> usize {
+        self.size
+    }
+
+    async fn advance(&mut self, advance: usize, size: usize) -> Option<&[T]> {
+        assert!(
+            advance <= self.size,
+            "cannot advance past end of read buffer"
+        );
+        assert!(
+            size <= (self.state.size - 1),
+            "cannot request read buffer larger than total buffer"
+        );
+
+        let head = if let Some(multi_source) = &self.multi_source {
+            self.state.advance_value(advance, &multi_source.head);
+            let heads = multi_source.heads.read().unwrap();
+            assert!(!heads.is_empty());
+            // Repeatedly find the earliest head until the store is consistent
+            loop {
+                let current_head = self.state.head.load(Ordering::AcqRel);
+                let mut earliest_head = std::usize::MAX;
+                let mut smallest_distance = std::usize::MAX;
+                for head in heads.iter() {
+                    let this_head = head.load(Ordering::AcqRel);
+                    let this_distance = self.state.distance(current_head, this_head);
+                    if this_distance < smallest_distance {
+                        earliest_head = this_head;
+                        smallest_distance = this_distance;
+                    }
+                }
+                let current =
+                    self.state
+                        .head
+                        .compare_and_swap(current_head, earliest_head, Ordering::AcqRel);
+                if current == current_head {
+                    break;
+                }
+            }
+            multi_source.head.load(Ordering::Relaxed)
+        } else {
+            // Directly update the head
+            self.state.advance_value(advance, &self.state.head);
+            self.state.head.load(Ordering::Relaxed)
+        };
+
+        // Notify sink that space is available
+        // If the sink is gone, we continue because there might still be data in the buffer
+        let _ = self.trigger_sender.try_send(());
+
+        // Wait for enough data to read
+        while self
+            .state
+            .distance(head, self.state.tail.load(Ordering::Relaxed))
+            < size
+        {
+            if self.trigger_receiver.recv().await.is_none() {
+                break;
+            }
+        }
+        self.size = std::cmp::min(
+            self.state
+                .distance(head, self.state.tail.load(Ordering::Relaxed)),
+            size,
+        );
+
+        // Return the slice
+        if self.size == 0 {
+            None
+        } else {
+            let ptr = self.state.head_ptr();
+            Some(unsafe { std::slice::from_raw_parts(ptr, self.size) })
+        }
     }
 }
--- src/buffer/spsc.rs
+++ src/buffer/spsc.rs
@@ -1,10 +1,10 @@
 //! A single-producer, single-consumer async buffer.
 
-use crate::buffer::State;
+use crate::buffer::{MultiSender, SinkImpl, SourceImpl, State};
 use crate::stream::{Sink, Source};
 use async_trait::async_trait;
 use std::sync::Arc;
-use tokio::sync::mpsc::{channel, Receiver, Sender};
+use tokio::sync::mpsc::channel;
 
 /// Creates a single-producer, single-consumer async buffer.
 ///
@@ -22,16 +22,21 @@ pub fn buffer<T: Send + Sync + Default + 'static>(
 
     (
         BufferSink::<T> {
-            state: state.clone(),
-            size: 0,
-            trigger_receiver: sink_receiver,
-            trigger_sender: sink_sender,
+            sink: SinkImpl {
+                state: state.clone(),
+                size: 0,
+                trigger_receiver: sink_receiver,
+                trigger_sender: MultiSender::Single(Box::new(sink_sender)),
+            },
         },
         BufferSource::<T> {
-            state,
-            size: 0,
-            trigger_receiver: source_receiver,
-            trigger_sender: source_sender,
+            source: SourceImpl {
+                state,
+                size: 0,
+                trigger_receiver: source_receiver,
+                trigger_sender: source_sender,
+                multi_source: None,
+            },
         },
     )
 }
@@ -42,10 +47,7 @@ pub fn buffer<T: Send + Sync + Default + 'static>(
 ///
 /// [`buffer`]: fn.buffer.html
 pub struct BufferSink<T: Send + Sync + 'static> {
-    state: Arc<State<T>>,
-    size: usize,
-    trigger_receiver: Receiver<()>,
-    trigger_sender: Sender<()>,
+    sink: SinkImpl<T>,
 }
 
 #[async_trait]
@@ -53,41 +55,11 @@ impl<T: Send + Sync + 'static> Sink for BufferSink<T> {
     type Item = T;
 
     fn len(&self) -> usize {
-        self.size
+        self.sink.len()
     }
 
     async fn advance(&mut self, advance: usize, size: usize) -> Option<&mut [T]> {
-        assert!(
-            advance <= self.size,
-            "cannot advance past end of write buffer"
-        );
-        assert!(
-            size <= (self.state.size - 1),
-            "cannot request write buffer larger than total buffer"
-        );
-        self.state.advance_tail(advance);
-
-        // Notify source that data is available
-        // If the source is gone, there's no point sinking more data
-        if let Err(e) = self.trigger_sender.try_send(()) {
-            if e.is_closed() {
-                self.size = 0;
-                return None;
-            }
-        }
-
-        // Wait for enough room to advance
-        while self.state.writable_len() < size {
-            if self.trigger_receiver.recv().await.is_none() {
-                self.size = 0;
-                return None;
-            }
-        }
-        self.size = size;
-
-        // Return the mutable slice
-        let ptr = self.state.tail_ptr();
-        Some(unsafe { std::slice::from_raw_parts_mut(ptr, self.size) })
+        self.sink.advance(advance, size).await
     }
 }
 
@@ -97,10 +69,7 @@ impl<T: Send + Sync + 'static> Sink for BufferSink<T> {
 ///
 /// [`buffer`]: fn.buffer.html
 pub struct BufferSource<T> {
-    state: Arc<State<T>>,
-    size: usize,
-    trigger_receiver: Receiver<()>,
-    trigger_sender: Sender<()>,
+    source: SourceImpl<T>,
 }
 
 #[async_trait]
@@ -108,39 +77,10 @@ impl<T: Send + Sync + 'static> Source for BufferSource<T> {
     type Item = T;
 
     fn len(&self) -> usize {
-        self.size
+        self.source.len()
     }
 
     async fn advance(&mut self, advance: usize, size: usize) -> Option<&[T]> {
-        assert!(
-            advance <= self.size,
-            "cannot advance past end of read buffer"
-        );
-        assert!(
-            size <= (self.state.size - 1),
-            "cannot request read buffer larger than total buffer"
-        );
-        self.state.advance_head(advance);
-
-        // Notify sink that space is available
-        // If the sink is gone, we continue because there might still be data in the buffer
-        let _ = self.trigger_sender.try_send(());
-
-        // Wait for enough data to read
-        while self.state.readable_len() < size {
-            if self.trigger_receiver.recv().await.is_none() {
-                if self.state.readable_len() == 0 {
-                    self.size = 0;
-                    return None;
-                } else {
-                    break;
-                }
-            }
-        }
-        self.size = std::cmp::min(self.state.readable_len(), size);
-
-        // Return the slice
-        let ptr = self.state.head_ptr();
-        Some(unsafe { std::slice::from_raw_parts(ptr, self.size) })
+        self.source.advance(advance, size).await
     }
 }
