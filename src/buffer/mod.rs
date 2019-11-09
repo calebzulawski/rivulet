5e734f0b8ccde1beb2f4aa9fd1beb2310e8bf021
Caleb Zulawski <caleb.zulawski@gmail.com>
2019-11-09 10:17:17 -0500

    Move buffer state to module level

--- src/buffer/mod.rs
+++ src/buffer/mod.rs
@@ -1,3 +1,63 @@
 //! Buffers for temporarily caching data.
 
 pub mod spsc;
+
+use slice_deque::Buffer;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+struct State<T> {
+    buffer: Buffer<T>,
+    size: usize,
+    head: AtomicUsize,
+    tail: AtomicUsize,
+}
+
+impl<T: Default> State<T> {
+    fn new(min_size: usize) -> Self {
+        // Initialize the buffer memory
+        // The +1 ensures there's room for a marker element (to indicate the difference between
+        // empty and full
+        let buffer = Buffer::<T>::uninitialized(2 * (min_size + 1)).unwrap();
+        unsafe {
+            for index in 0..(buffer.len() / 2) {
+                buffer.ptr().add(index).write(T::default());
+            }
+        }
+
+        Self {
+            size: buffer.len() / 2,
+            buffer,
+            head: AtomicUsize::new(0),
+            tail: AtomicUsize::new(0),
+        }
+    }
+}
+
+impl<T> State<T> {
+    fn head_ptr(&self) -> *const T {
+        unsafe { self.buffer.ptr().add(self.head.load(Ordering::Relaxed)) }
+    }
+
+    fn tail_ptr(&self) -> *mut T {
+        unsafe { self.buffer.ptr().add(self.tail.load(Ordering::Relaxed)) }
+    }
+
+    fn advance_tail(&self, advance: usize) {
+        let new_tail = (self.tail.load(Ordering::Relaxed) + advance) % self.size;
+        self.tail.store(new_tail, Ordering::Relaxed);
+    }
+
+    fn advance_head(&self, advance: usize) {
+        let new_head = (self.head.load(Ordering::Relaxed) + advance) % self.size;
+        self.head.store(new_head, Ordering::Relaxed);
+    }
+
+    fn writable_len(&self) -> usize {
+        self.size - self.readable_len() - 1
+    }
+
+    fn readable_len(&self) -> usize {
+        (self.tail.load(Ordering::Relaxed) + self.size - self.head.load(Ordering::Relaxed))
+            % self.size
+    }
+}
--- src/buffer/spsc.rs
+++ src/buffer/spsc.rs
@@ -1,12 +1,9 @@
 //! A single-producer, single-consumer async buffer.
 
+use crate::buffer::State;
 use crate::stream::{Sink, Source};
 use async_trait::async_trait;
-use slice_deque::Buffer;
-use std::sync::{
-    atomic::{AtomicUsize, Ordering},
-    Arc,
-};
+use std::sync::Arc;
 use tokio::sync::mpsc::{channel, Receiver, Sender};
 
 /// Creates a single-producer, single-consumer async buffer.
@@ -37,61 +34,6 @@ pub fn buffer<T: Send + Sync + Default + 'static>(
             trigger_sender: source_sender,
         },
     )
-}
-
-struct State<T> {
-    buffer: Buffer<T>,
-    size: usize,
-    head: AtomicUsize,
-    tail: AtomicUsize,
-}
-
-impl<T: Default> State<T> {
-    fn new(min_size: usize) -> Self {
-        // Initialize the buffer memory
-        let buffer = Buffer::<T>::uninitialized(2 * (min_size + 1)).unwrap();
-        unsafe {
-            for index in 0..(buffer.len() / 2) {
-                buffer.ptr().add(index).write(T::default());
-            }
-        }
-
-        Self {
-            size: buffer.len() / 2,
-            buffer,
-            head: AtomicUsize::new(0),
-            tail: AtomicUsize::new(0),
-        }
-    }
-}
-
-impl<T> State<T> {
-    fn head_ptr(&self) -> *const T {
-        unsafe { self.buffer.ptr().add(self.head.load(Ordering::Relaxed)) }
-    }
-
-    fn tail_ptr(&self) -> *mut T {
-        unsafe { self.buffer.ptr().add(self.tail.load(Ordering::Relaxed)) }
-    }
-
-    fn advance_tail(&self, advance: usize) {
-        let new_tail = (self.tail.load(Ordering::Relaxed) + advance) % self.size;
-        self.tail.store(new_tail, Ordering::Relaxed);
-    }
-
-    fn advance_head(&self, advance: usize) {
-        let new_head = (self.head.load(Ordering::Relaxed) + advance) % self.size;
-        self.head.store(new_head, Ordering::Relaxed);
-    }
-
-    fn writable_len(&self) -> usize {
-        self.size - self.readable_len() - 1
-    }
-
-    fn readable_len(&self) -> usize {
-        (self.tail.load(Ordering::Relaxed) + self.size - self.head.load(Ordering::Relaxed))
-            % self.size
-    }
 }
 
 /// Write values to the associated `BufferSource`.
