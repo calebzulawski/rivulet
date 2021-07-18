49d57230480fafa55277a8b0272a60e5ed64c14d
calebzulawski <calebzulawski@users.noreply.github.com>
2021-07-18 13:59:35 +0000

    deploy: 823adcb40f73221d1f386b1aa44307b122fc394f

---
+++ rivulet/lazy/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Lazy","A lazy-initialized view."]]});
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["rivulet"] = {"name":"","files":["base.rs","circular_buffer.rs","error.rs","io.rs","lib.rs","slice.rs"]};
+sourcesIndex["rivulet"] = {"name":"","files":["base.rs","circular_buffer.rs","error.rs","io.rs","lazy.rs","lib.rs","slice.rs"]};
 createSourceSidebar();
