e8db59b9af5dc771bab64330f63b38101e3dfa5b
calebzulawski <calebzulawski@users.noreply.github.com>
2021-07-31 05:06:55 +0000

    deploy: 205147782fa1fee171be4cb619d6d4fa550862ce

--- rivulet/lazy/sidebar-items.js
+++ rivulet/lazy/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Lazy","A lazy-initialized view."]]});
+initSidebarItems({"fn":[["lazy_channel","Create a lazy-initialized channel."]],"struct":[["Lazy","A lazy-initialized view."],["LazyChannelSink","A sink created by [`lazy_channel`]."],["LazyChannelSource","A source created by [`lazy_channel`]."]]});
