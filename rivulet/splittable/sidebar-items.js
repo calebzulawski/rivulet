2b7469a37298ab0f7e7b3c0bb33eb4edebe1052d
calebzulawski <calebzulawski@users.noreply.github.com>
2021-11-02 04:03:13 +0000

    deploy: b4c9934cb76eed88903eee36e2a08a4b5c936657

---
+++ rivulet/splittable/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Source",""],["Split",""]],"trait":[["Splittable",""],["SplittableSource",""],["SplittableSourceMut",""]]});
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["rivulet"] = {"name":"","files":["base.rs","circular_buffer.rs","error.rs","io.rs","lazy.rs","lib.rs","slice.rs"]};
+sourcesIndex["rivulet"] = {"name":"","dirs":[{"name":"splittable","files":["source.rs","split.rs"]}],"files":["base.rs","circular_buffer.rs","error.rs","io.rs","lazy.rs","lib.rs","slice.rs","splittable.rs"]};
 createSourceSidebar();
