49f3590cdb453121fadcc7268eade0f0c652e0a3
calebzulawski <calebzulawski@users.noreply.github.com>
2021-11-04 00:51:45 +0000

    deploy: 44b2798bea699d08e39641319bdac8b440d3cc1b

---
+++ rivulet/splittable/implementation/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"trait":[["IntoSplittableSource","Trait for constructing types that implement [`SplittableSource`]."],["SplittableSource","Similar to [`crate::Source`], but can be used with multiple readers."],["SplittableSourceMut","A mutable source that can be used with multiple readers."]]});
