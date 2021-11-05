f4d43db9beffe738e759dab26ff6f02182959d5a
Caleb Zulawski <caleb.zulawski@gmail.com>
2021-11-05 05:30:32 +0000

    Disable lint

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(not(feature = "std"), no_std)]
-#![deny(rustdoc::broken_intra_doc_links, missing_docs)]
+#![deny(missing_docs)]
 //! Rivulet provides tools for creating and processing asynchronous streams of contiguous data.
 
 pub mod circular_buffer;
