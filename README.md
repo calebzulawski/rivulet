9d4ff8905b6adf571d1ec0062c625d10201d6d4b
Caleb Zulawski <caleb.zulawski@gmail.com>
2021-11-06 05:58:54 +0000

    Add some documentation

---
+++ README.md
@@ -0,0 +1,20 @@
+Rivulet
+=======
+[![Build Status](https://github.com/calebzulawski/rivulet/workflows/Build/badge.svg?branch=master)](https://github.com/calebzulawski/rivulet/actions)
+![Rustc Version 1.45+](https://img.shields.io/badge/rustc-1.45+-lightgray.svg)
+[![License](https://img.shields.io/crates/l/rivulet)](https://crates.io/crates/rivulet)
+[![Crates.io](https://img.shields.io/crates/v/rivulet)](https://crates.io/crates/rivulet)
+[![Rust Documentation](https://img.shields.io/badge/api-rustdoc-blue.svg)](https://docs.rs/rivulet)
+
+Rivulet is a library for creating asynchronous pipelines of contiguous data.
+
+Main features, at a glance:
+
+* **Asynchronous**: Pipeline components are `async`-aware, allowing more control over task priority and data backpressure.
+* **Contiguous views**: Data is always contiguous, accessible by a single slice.
+* **Modular**: Pipelines can be combined and reused using common interfaces.
+
+## License
+Multiversion is distributed under the terms of both the MIT license and the Apache License (Version 2.0).
+
+See [LICENSE-APACHE](LICENSE-APACHE) and [LICENSE-MIT](LICENSE-MIT) for details.
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,54 @@
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![deny(missing_docs)]
-//! Rivulet provides tools for creating and processing asynchronous streams of contiguous data.
+//! Rivulet is a library for creating asynchronous pipelines of contiguous data.
+//!
+//! Main features, at a glance:
+//!
+//! * **Asynchronous**: Pipeline components are `async`-aware, allowing more control over task priority and data backpressure.
+//! * **Contiguous views**: Data is always contiguous, accessible by a single slice.
+//! * **Modular**: Pipelines can be combined and reused using common interfaces.
+//!
+//! # Example
+//!
+//! Let's create a simple pipeline for byte-reversing.
+//!
+//! ```
+//! use rivulet::{Source, Sink};
+//! use futures::future::TryFutureExt;
+//!
+//! async fn reverse_bytes(
+//!     mut source: impl Source<Item=u8> + Unpin,
+//!     mut sink: impl Sink<Item=u8> + Unpin,
+//!     width: usize
+//! ) -> Result<(), &'static str> {
+//!     loop {
+//!         // Wait for the input and output to have `width` elements available.
+//!         tokio::try_join!(
+//!             source.grant(width).map_err(|_| "we got an input error!"),
+//!             sink.grant(width).map_err(|_| "we got an output error!"),
+//!         )?;
+//!
+//!         // The view could be longer (if available) or shorter (if the stream closed)
+//!         let input = source.view();
+//!         let output = sink.view_mut();
+//!         if input.len() < width || output.len() < width {
+//!             break Ok(())
+//!         }
+//!         let input = &input[..width];
+//!         let output = &mut output[..width];
+//!
+//!         // Copy the elements from the source into the sink, in reverse
+//!         for (i, o) in input.iter().rev().zip(output.iter_mut()) {
+//!             *o = *i;
+//!         }
+//!
+//!         // We've written all our data, so release our current views and advance the streams
+//!         source.release(width);
+//!         sink.release(width);
+//!     }
+//! }
+//! ```
 
 pub mod circular_buffer;
 pub mod error;
